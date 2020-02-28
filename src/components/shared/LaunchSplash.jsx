1fb0d32e322faef7fb6368983583c3d11b7e3bce
zetcosta <zetcosta12@gmail.com>
2020-02-28 22:28:20 +0100

    fixed issues

--- src/components/shared/LaunchSplash.jsx
+++ src/components/shared/LaunchSplash.jsx
@@ -6,7 +6,7 @@ import LaunchForm from "../shared/LaunchForm";
 import launchsplash from "../../assets/launch_splash.jpg";
 
 const Wrapper = styled.div`
-  height: calc(100vh - 10px);
+  height: calc(100vh - 30px);
   background: rgb(242,245,247);
 `;
 
