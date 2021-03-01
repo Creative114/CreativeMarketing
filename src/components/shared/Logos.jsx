1a94d5eb6d29024611db78f184b80725766f3f80
relecon <shawn.condra@relecon.com>
2021-03-01 10:19:43 -0500

    logo update

--- src/components/shared/Logos.jsx
+++ src/components/shared/Logos.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-//import logo1 from '../../assets/logo1.jpg';
+// import logo1 from '../../assets/logo1.jpg';
 // import logo2 from '../../assets/logo2.jpg';
 import logo3 from '../../assets/logo3.jpg';
 import logo4 from '../../assets/logo4.jpg';
